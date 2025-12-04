331505cf9659e4b3b19712ba050922b30ee89aa7
Yannick <yk.105m@gmail.com>
2025-12-04 14:24:59 +0100

    correct metadata in Dictionnaire page due to App rooter's rules for vercel

---
+++ src/app/Education/Dictionnaire/layout.tsx
@@ -0,0 +1,24 @@
+export const metadata = {
+  title: "Dictionnaire Syntaxerrorisme — Analyse de Code & Détection d’Erreurs",
+  description:
+    "Le Dictionnaire Syntaxerrorisme analyse votre code, détecte les erreurs, explique les corrections et vous aide à écrire un code propre, clair et structuré. Compatible JS, TS, Python, C++, Java et plus.",
+  robots: {
+    index: true,
+    follow: true,
+  },
+  keywords: [
+    "analyse code",
+    "syntaxerrorisme",
+    "erreurs code",
+    "javascript",
+    "python",
+    "c++",
+    "java",
+    "intelligence artificielle",
+    "linter",
+    
+  ],
+  alternates: {
+    canonical: "https://syntaxerrorisme-omega.vercel.app/dictionnaire",
+  },
+};
--- src/app/Education/Dictionnaire/page.tsx
+++ src/app/Education/Dictionnaire/page.tsx
@@ -4,30 +4,6 @@ import { useState } from "react";
 import Header from "@/feature/Header";
 import Footer from "@/feature/Footer";
 
-export const metadata = {
-  title: "Dictionnaire Syntaxerrorisme — Analyse de Code & Détection d’Erreurs",
-  description:
-    "Le Dictionnaire Syntaxerrorisme analyse votre code, détecte les erreurs, explique les corrections et vous aide à écrire un code propre, clair et structuré. Compatible JS, TS, Python, C++, Java et plus.",
-  robots: {
-    index: true,
-    follow: true,
-  },
-  keywords: [
-    "analyse code",
-    "syntaxerrorisme",
-    "erreurs code",
-    "javascript",
-    "python",
-    "c++",
-    "java",
-    "intelligence artificielle",
-    "linter",
-    
-  ],
-  alternates: {
-    canonical: "https://syntaxerrorisme-omega.vercel.app/dictionnaire",
-  },
-};
 
 export default function DictionnairePage() {
   const [code, setCode] = useState("");
