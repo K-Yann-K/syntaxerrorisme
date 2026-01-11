f3a39941119975e065ed80f493fde873e2022ba1
Yannick <yk.105m@gmail.com>
2026-01-11 02:02:01 +0100

    trying to fix contrast'colors from header

--- src/feature/Header.tsx
+++ src/feature/Header.tsx
@@ -20,8 +20,8 @@ export default function Header() {
             <header className="relative group">
                 <Link href={"/"}>
                     <h1 className="text-strikemaster-700 glitch font-black text-lg flex items-center gap-2">
-                        <aside className="relative flex items-center justify-center w-6 h-6 rounded-full bg-strikemaster-600/10">
-                            <span className="absolute w-2 h-2 bg-strikemaster-700 rounded-full animate-pulse"></span>
+                        <aside className="relative flex items-center justify-center w-6 h-6 rounded-full bg-strikemaster-900/10">
+                            <span className="absolute w-2 h-2 bg-strikemaster-800 rounded-full animate-pulse"></span>
                         </aside>
                         Syntaxerrorisme
                     </h1>
